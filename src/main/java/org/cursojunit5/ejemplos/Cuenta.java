bb62dff6efdcc98f14ddfd67f264f2008ce9060b
bgr8594 <bgr8594@gmail.com>
2023-08-12 01:39:10 -0600

    Proyecto JUNIT5
    Proyecto en blanco

---
+++ src/main/java/org/cursojunit5/ejemplos/Cuenta.java
@@ -0,0 +1,36 @@
+package org.cursojunit5.ejemplos;
+
+import java.math.BigDecimal;
+
+public class Cuenta {
+    private String persona;
+    private BigDecimal saldo;
+
+    public String getPersona() {
+        return persona;
+    }
+
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Cuenta)){
+            return false;
+        }
+        Cuenta c = (Cuenta) obj;
+        if(this.persona == null || this.saldo == null){
+            return false;
+        }
+        return this.persona.equals(c.getPersona()) && this.saldo.equals(c.getSaldo());
+    }
+}
